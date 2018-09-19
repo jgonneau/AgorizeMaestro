9d8706872a9b09d56742e601cc69294dee059b3c
NaweedSufranj <sufranj.naweed@gmail.com>
2018-09-19 16:34:04 +0200

    integration des pages : home/connexion/inscription/dashboard

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+indexRouter.get('/connexion', function(req, res, next) {
+  res.render('connexion', { title: 'Express' });
+});
+
+indexRouter.get('/inscription', function(req, res, next) {
+  res.render('inscription', { title: 'Express' });
+});
+
+indexRouter.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Express' });
+});
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
